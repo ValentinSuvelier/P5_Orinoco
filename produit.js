af0cc8066d30dd03953d896970a9cb787bfd87cf
Valentin <valentin.suvelier@gmail.com>
2021-09-07 15:15:56 +0200

    Accueil + Produit

--- produit.js
+++ produit.js
@@ -1,8 +1,16 @@
+function getProduitId()
+{
+   
+    let id = new URL(location.href).searchParams.get("id");
+    return id;
+}
 function getArticles()
 /*crée une fonction permettant de récupérer les info de l'API, de les convertirs en format 'json' et d'afficher une
 alerte error si le fetch ne fonctionne pas*/
 {
-    return fetch("http://localhost:3000/api/teddies")
+    produitId = getProduitId();
+    urlProduit = "http://localhost:3000/api/teddies/"+produitId;
+    return fetch(urlProduit)
     .then(function(httpBodyResponse){
         return httpBodyResponse.json()
     })
@@ -15,34 +23,31 @@ function displayArticle(produit)
 {
     const template = document.getElementById("templateProduit");
     const clone = document.importNode(template.content, true);
-    /*récupère les éléments de l'API pour les caractéristique ciblées*/
+    //récupère les éléments de l'API pour les caractéristique ciblées
     clone.getElementById("ours_title").textContent = produit.name;
     clone.getElementById("ours_price").textContent = produit.price / 100 + ".00€";
     clone.querySelector(".ours_picture").setAttribute("src", produit.imageUrl);
     clone.getElementById("ours_body").textContent = produit.description;
     clone.getElementById("ours_link").href += `?id=${produit._id}`;
-
-    /*intègre le clone en récupérant "main" dans la page ainsi que son élément enfant "clone"*/
+    //intègre le clone en récupérant "main" dans la page ainsi que son élément enfant "clone"
     document.getElementById("main").appendChild(clone);
-}
 
+    document.querySelector(".colorsAdd").innerHTML += `
+    <select class="choosecolor" name="peluche" id="${produit._id}">
+        <option value=""> -- Choisir un coloris -- </option>
+    </select>`;
 
-   /* for(couleur of produit.colors)
+    for(couleur of produit.colors)
     {
         document.getElementById(produit._id).innerHTML += `
-        <option>${couleur}</option>`
+        <option>${couleur}</option>`;
     }
-    }*/
-
+}
 
     async function main()
 {
-    const articles = await getArticles()
-
-    for (produit of articles) 
-    {
+        const produit = await getArticles()
         displayArticle(produit)
-    }
 }
 
 main()
