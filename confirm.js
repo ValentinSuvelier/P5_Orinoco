92da5d22e3e27d02fcc13756470dfbb657d401ea
Valentin <valentin.suvelier@gmail.com>
2022-06-08 16:57:07 +0200

    suppression lignes inutiles

--- confirm.js
+++ confirm.js
@@ -1,7 +1,3 @@
-let confirmation = document.querySelector('.commande_id').textContent = localStorage.getItem('orderID');
-    //localStorage.clear();
-
-
 async function main()
 {
     var affichageProduit = JSON.parse(localStorage.getItem("panier"));
@@ -11,10 +7,9 @@ async function main()
         total = total + article.price;
     }
     console.log('prix total ' + total);
+    document.querySelector('.commande_id').textContent = localStorage.getItem('orderID');
     document.querySelector(".name").textContent = localStorage.getItem("panier");
     document.querySelector(".total").textContent = total + '.00€' + ' (+ 10.99€ de frais de port) soit ' + (total + 10.99 + '€');
-    let confirmation = document.querySelector('.commande_id').textContent = localStorage.getItem('orderID');
-    //localStorage.clear();
 }
 
 main()
